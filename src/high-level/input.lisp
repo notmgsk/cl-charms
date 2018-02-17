68a5a976c687e032e1e2098b3f77b4a8e800405b
Mark Skilbeck <markskilbeck@gmail.com>
2018-02-17 20:18:38 +0000

    Small doc fix

--- src/high-level/input.lisp
+++ src/high-level/input.lisp
@@ -5,7 +5,7 @@
 (in-package #:cl-charms)
 
 (defun get-char (window &key ignore-error)
-  "Get a character from the window WINDOW. In the event a character is not ready or could not be returned, thensignal an error. If IGNORE-ERROR is T, then instead return NIL."
+  "Get a character from the window WINDOW. In the event a character is not ready or could not be returned, then signal an error. If IGNORE-ERROR is T, then instead return NIL."
   (let ((c (charms/ll:wgetch (window-pointer (resolve-window window)))))
     (cond
       ((not (eql c charms/ll:ERR)) (c-char-to-character c))
